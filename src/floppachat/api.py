631b86b7a18e78690137d9ddad327aa120d81a49
Ben O'Neill <ben@benoneill.xyz>
2022-01-28 08:55:11 -0500

    restructure messages.py and api.py

--- src/floppachat/api.py
+++ src/floppachat/api.py
@@ -3,13 +3,9 @@ from flask_restful import Api
 
 import users, messages
 
-class ChatAPI:
-    def __init__(self):
-        users.Users.create_table()
-        pass
-    def run(self):
-        app = Flask('FloppaChat')
-        api = Api(app)
-        api.add_resource(users.Users, '/users')
-        api.add_resource(messages.Messages, '/messages')
-        app.run()
+if __name__ == '__main__':
+    app = Flask('FloppaChat')
+    api = Api(app)
+    api.add_resource(users.Users, '/users')
+    api.add_resource(messages.Messages, '/messages')
+    app.run()
--- src/floppachat/messages.py
+++ src/floppachat/messages.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource
+from db import *
 
 class Messages(Resource):
     def __init__(self):
